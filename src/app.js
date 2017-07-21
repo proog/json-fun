c2f2bed4c420c07bdc99ee9df0b4792456f9fafd
Per Mortensen <per@permortensen.com>
2017-07-21 23:28:15 +0200

    Add native formatting and more stuff

--- src/app.js
+++ src/app.js
@@ -5,39 +5,20 @@ let app = new Vue({
     formatted: '',
     indent: 2,
     live: true,
-    compact: true
+    compact: true,
+    error: false,
+    formatter: 'native'
   },
   watch: {
     input: watcher,
     indent: watcher,
     live: watcher,
-    compact: watcher
+    compact: watcher,
+    formatter: watcher
   },
   methods: {
-    format: function () {
-      if (this.input.trim() === '') {
-        this.formatted = '';
-        return;
-      }
-
-      try {
-        let data = JSON.parse(this.input)
-          , formatter = new Formatter(this.indent, this.compact);
-        this.formatted = formatter.format(data);
-      }
-      catch (e) {
-        this.formatted = makeParseError(e, this.input);
-      }
-    },
-    copyOutput: function () {
-      let copyBuffer = document.getElementById('copy-buffer');
-
-      copyBuffer.style.display = 'block';
-      copyBuffer.select();
-      document.execCommand('copy');
-      copyBuffer.blur();
-      copyBuffer.style.display = 'none';
-    }
+    format: format,
+    copyOutput: copyOutput
   }
 });
 
@@ -46,18 +27,56 @@ function watcher() {
     this.format();
 }
 
+function format() {
+  if (this.input.trim() === '') {
+    this.formatted = '';
+    this.error = false;
+    return;
+  }
+
+  try {
+    let data = JSON.parse(this.input);
+    this.formatted = this.formatter === 'custom'
+      ? formatCustom(data, this.indent, this.compact)
+      : formatNative(data, this.indent);
+    this.error = false;
+  }
+  catch (e) {
+    this.formatted = makeParseError(e, this.input);
+    this.error = true;
+  }
+}
+
+function formatNative(data, indent) {
+  return JSON.stringify(data, null, _.repeat(' ', indent));
+}
+
+function formatCustom(data, indent, compact) {
+  return new Formatter(indent, compact).format(data);
+}
+
+function copyOutput() {
+  let copyBuffer = document.getElementById('copy-buffer');
+
+  copyBuffer.style.display = 'block';
+  copyBuffer.select();
+  document.execCommand('copy');
+  copyBuffer.blur();
+  copyBuffer.style.display = 'none';
+}
+
 function makeParseError(error, input) {
   let match = / at position (\d+)/.exec(error);
 
   if (!match || match.length !== 2)
-    return `Parse error: ${error}`;
+    return error;
 
   let position = parseInt(match[1])
     , start = Math.max(0, position - 10)
-    , snippet = input.substr(start, 20);
+    , snippet = input.substr(start, 20)
+    , marker = _.repeat(' ', position - start) + '^';
 
-  return 'Something wasn\'t valid around here\n'
-    + `  ${snippet}\n\n`
-    + 'The error was\n'
-    + `  ${error}`;
+  return error + '\n\n'
+    + snippet + '\n'
+    + (_.includes(snippet, '\n') ? '' : marker);
 }
