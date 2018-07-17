45b94348cbbd11304f54e48fb24b7c80dc86aa50
Per Mortensen <per@permortensen.com>
2018-07-17 22:49:16 +0200

    Fix typo

--- src/JsonInfo.vue
+++ src/JsonInfo.vue
@@ -10,7 +10,7 @@
   <ul>
     <li>JSON: <code>0.00</code> becomes <code>0</code></li>
     <li>JSON: <code>0.0000001</code> becomes <code>1e-7</code></li>
-    <li>XML: leading and trailing whitespace in will be <strong>ignored</strong></li>
+    <li>XML: leading and trailing whitespace will be <strong>ignored</strong></li>
     <li>XML: empty nodes will be <strong>self-closed</strong></li>
     <li>XML: internal DTDs in !DOCTYPE will be <strong>ignored</strong></li>
   </ul>
