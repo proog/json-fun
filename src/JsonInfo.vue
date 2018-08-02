8e5bd2da150752af00b3734cc8c784d4cff28ffb
Per Mortensen <per@permortensen.com>
2018-08-02 18:13:35 +0200

    Update welcome page

--- src/JsonInfo.vue
+++ src/JsonInfo.vue
@@ -1,18 +1,23 @@
 <template>
-<div class="px-md-5 py-3">
-  <h1>Make it readable!</h1>
-  <p>This tool formats JSON (and XML) for human readability only.</p>
-  <p>
-    Please do not use this tool for use cases where precision is critical.
-    It parses and re-renders the input and will often <em>not</em> produce a 1:1 copy.
-  </p>
-  <p>Examples:</p>
-  <ul>
-    <li>JSON: <code>0.00</code> becomes <code>0</code></li>
-    <li>JSON: <code>0.0000001</code> becomes <code>1e-7</code></li>
-    <li>XML: leading and trailing whitespace will be <strong>ignored</strong></li>
-    <li>XML: empty nodes will be <strong>self-closed</strong></li>
-    <li>XML: internal DTDs in !DOCTYPE will be <strong>ignored</strong></li>
-  </ul>
+<div class="h-100 d-flex flex-column">
+  <div class="px-md-5 pt-3 h-100 relative overflow-auto">
+    <h1>Make it readable!</h1>
+    <p>This tool formats JSON (and XML) for human readability only.</p>
+    <p>
+      Don't use this tool for use cases where precision is critical.
+      It parses and re-renders the input and will often <em>not</em> produce a 1:1 copy.
+    </p>
+    <p>Examples:</p>
+    <ul>
+      <li>JSON: <code>0.00</code> becomes <code>0</code></li>
+      <li>JSON: <code>0.0000001</code> becomes <code>1e-7</code></li>
+      <li>XML: leading and trailing whitespace will be <em>ignored</em></li>
+      <li>XML: empty nodes will be <em>self-closed</em></li>
+      <li>XML: internal DTDs in !DOCTYPE will be <em>ignored</em></li>
+    </ul>
+  </div>
+  <div class="mt-2 text-center">
+    A project by <a href="https://permortensen.com">Per</a>
+  </div>
 </div>
 </template>
