df3db42d1947eaae193a83891213de9c22078e10
Per Mortensen <per@permortensen.com>
2021-01-27 22:22:52 +0100

    Add transition

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { connect } from "react-redux";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import FormattedOutput from "./FormattedOutput";
 import InputForm from "./InputForm";
 import JsonInfo from "./JsonInfo";
@@ -9,9 +10,17 @@ function App({ formatted }) {
       <div className="h-full flex flex-wrap">
         <InputForm className="w-full h-full flex flex-col py-4 md:w-1/3 md:pr-2" />
         <div className="w-full h-full py-4 md:w-2/3 md:pl-2">
-          <transition mode="out-in">
-            {formatted ? <FormattedOutput /> : <JsonInfo />}
-          </transition>
+          <SwitchTransition mode="out-in">
+            <CSSTransition
+              key={!!formatted}
+              addEndListener={(node, done) =>
+                node.addEventListener("transitionend", done, false)
+              }
+              classNames="fade"
+            >
+              {formatted ? <FormattedOutput /> : <JsonInfo />}
+            </CSSTransition>
+          </SwitchTransition>
         </div>
       </div>
     </div>
