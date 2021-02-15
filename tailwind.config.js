79cf091523e9594ad6d42dc8f13f0d015cfa849f
Per Mortensen <per@permortensen.com>
2021-02-15 17:12:50 +0100

    Update tailwind to 2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
 module.exports = {
-  future: {
-    purgeLayersByDefault: true,
-    removeDeprecatedGapUtilities: true,
-  },
   purge: ["./public/index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
