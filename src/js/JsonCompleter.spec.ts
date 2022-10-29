a8966e416e01d7ab7f53e1d12302b5e658e40a55
Per Mortensen <per@permortensen.com>
2022-10-29 11:48:38 +0200

    Convert to vite and vitest

--- src/js/JsonCompleter.spec.ts
+++ src/js/JsonCompleter.spec.ts
@@ -1,67 +1,64 @@
-import { test } from "uvu";
-import * as assert from "uvu/assert";
-import JsonCompleter from "./JsonCompleter.js";
+import { expect, test } from "vitest";
+import JsonCompleter from "./JsonCompleter";
 
 const completer = new JsonCompleter();
 
 test("Closes object", () => {
   const input = '{"k":"s"';
   const output = completer.complete(input);
-  assert.is(output, '{"k":"s"}');
+  expect(output).toBe('{"k":"s"}');
 });
 
 test("Closes string + object", () => {
   const input = '{"k":"s';
   const output = completer.complete(input);
-  assert.is(output, '{"k":"s"}');
+  expect(output).toBe('{"k":"s"}');
 });
 
 test("Closes number + object", () => {
   const input = '{"k":2.5';
   const output = completer.complete(input);
-  assert.is(output, '{"k":2.5}');
+  expect(output).toBe('{"k":2.5}');
 });
 
 test("Closes bool + object", () => {
   const input = '{"k":true';
   const output = completer.complete(input);
-  assert.is(output, '{"k":true}');
+  expect(output).toBe('{"k":true}');
 });
 
 test("Closes string + array + object", () => {
   const input = '{"k":["s';
   const output = completer.complete(input);
-  assert.is(output, '{"k":["s"]}');
+  expect(output).toBe('{"k":["s"]}');
 });
 
 test("Closes string with JSON tokens in it", () => {
   const input = '"[{';
   const output = completer.complete(input);
-  assert.is(output, '"[{"');
+  expect(output).toBe('"[{"');
 });
 
 test("Closes string with escaped string token in it", () => {
   const input = '"\\"';
   const output = completer.complete(input);
-  assert.is(output, '"\\""');
+  expect(output).toBe('"\\""');
 });
 
 test("Closes unfinished object key", () => {
   const input = '{"k';
   const output = completer.complete(input);
-  assert.is(output, '{"k":0}');
+  expect(output).toBe('{"k":0}');
 });
 
 test("Closes unfinished object key with escaped string token in it", () => {
   const input = '{"k\\"';
   const output = completer.complete(input);
-  assert.is(output, '{"k\\"":0}');
+  expect(output).toBe('{"k\\"":0}');
 });
 
 test("Closes unfinished object value", () => {
   const input = '{"k":';
   const output = completer.complete(input);
-  assert.is(output, '{"k":0}');
+  expect(output).toBe('{"k":0}');
 });
-
-test.run();
